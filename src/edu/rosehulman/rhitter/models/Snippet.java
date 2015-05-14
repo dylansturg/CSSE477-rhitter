fd69d0f82f01f1e789a65360a0c6c1dda0288519
Dylan Sturgeon <dylansturg@gmail.com>
2015-05-14 13:40:50 -0400

    basic data models

---
+++ src/edu/rosehulman/rhitter/models/Snippet.java
@@ -0,0 +1,11 @@
+package edu.rosehulman.rhitter.models;
+
+import java.util.Date;
+ 
+public class Snippet {
+	int id;
+	int publisherId;
+	
+	String text;
+	Date timestamp;
+}
---
+++ src/edu/rosehulman/rhitter/models/User.java
@@ -0,0 +1,7 @@
+package edu.rosehulman.rhitter.models;
+
+public class User {
+	int id;
+	String username;
+	String password;
+}
